cb3a536fbc13abe8ee70ed105d452469ad808a66
U2665ofEvil <79206160705@ya.ru>
2024-09-18 23:44:32 +0300

    work

--- main.py
+++ main.py
@@ -27,30 +27,30 @@ async def hello_start(_):  # функция для меня
 
 @dp.message_handler(commands='start')
 async def start(message):
-    print('Привет! Я бот помогающий твоему здоровью.')  # Задача "Бот поддержки (Начало)"
-    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=reply_markup)  # Задача "Он мне ответил!"
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=reply_markup)
 
 
-@dp.message_handler(text='Рассчитать')  # Задача "Цепочка вопросов
+@dp.message_handler(text='Рассчитать')
 async def start_calories(message):
     await set_age(message)
 
-@dp.message_handler(state=UserState.age)  # Задача "Цепочка вопросов
+@dp.message_handler(state=UserState.age)
 async def continue_calories(message, state):
     await set_growth(message, state)
 
-@dp.message_handler(state=UserState.growth)  # Задача "Цепочка вопросов
+@dp.message_handler(state=UserState.growth)
 async def continue_calories(message, state):
     await set_weight(message, state)
 
-@dp.message_handler(state=UserState.weight)  # Задача "Цепочка вопросов
+@dp.message_handler(state=UserState.weight)
 async def complete_calories(message, state):
     await send_calories(message, state)
 
 @dp.message_handler()
 async def all_massages(message):
-    print('Введите команду /start, чтобы начать общение.')  # Задача "Бот поддержки (Начало)"
-    await message.answer('Введите команду /start, чтобы начать общение.')  # Задача "Он мне ответил!"
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
